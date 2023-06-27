4aa7dd34ca01fc115509487cd3f72d4a0b21916f
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-06-27 21:47:35 +0100

    webcrawler

---
+++ webcrawler.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+def trade_spider(max_pages):
+    page = 1
+    while page < max_pages:
+       url = 'https://buybetter.ng/product-category/bath-body/page/' + str(page)
+       source_code = requests.get(url)
+       plain_text = source_code.text
+       soup = BeautifulSoup(plain_text)
+
